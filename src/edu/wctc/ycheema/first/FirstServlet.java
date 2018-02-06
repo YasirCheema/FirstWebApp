9ea43e941485ff59eceb779ed024e62f7b0c56db
YasirCheema <ycheema@my.wctc.edu>
2018-02-05 20:25:28 -0600

    initial commit

---
+++ src/edu/wctc/ycheema/first/FirstServlet.java
@@ -0,0 +1,14 @@
+package edu.wctc.ycheema.first;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "FirstServlet")
+public class FirstServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
